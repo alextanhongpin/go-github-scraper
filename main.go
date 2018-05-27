f2ebbe26077a3a00d9f95584bae7e2490e056076
alextanhongpin <alextan@seekasia.com>
2018-05-27 18:14:58 +0800

    chore: add profiling

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"log"
+	"net/http"
+	"net/http/pprof"
 
 	"github.com/alextanhongpin/go-github-scraper/api/github"
 	"github.com/alextanhongpin/go-github-scraper/internal/database"
@@ -17,6 +19,7 @@ import (
 )
 
 func main() {
+	// Setup environment variables
 	port := flag.String("port", ":8080", "The tcp port of the application")
 	githubToken := flag.String("github_token", "", "The Github's access token used to make calls to the GraphQL endpoint")
 	githubURI := flag.String("github_uri", "https://api.github.com/graphql", "The Github's GraphQL endpoint")
@@ -41,65 +44,36 @@ func main() {
 	usvc := usersvc.New(db)
 	gsvc := github.New(*githubToken, *githubURI)
 
-	// users, err := usvc.FindLastFetched(10)
-	// if err != nil {
-	// 	log.Println("fail to fetch users", err)
-	// }
-	// log.Printf("%#v\n", users)
-
-	// if err := usvc.Drop(); err != nil {
-	// 	log.Fatal("fail to drop users", err)
-	// }
-	// if err := rsvc.Drop(); err != nil {
-	// 	log.Fatal("fail to drop repos", err)
-	// }
-	if count, err := usvc.Count(); err != nil {
-		log.Fatal(err)
-	} else {
-		log.Printf("user count: %#v", count)
-	}
-
-	if count, err := rsvc.ReposByLanguage("JavaScript", 20); err != nil {
-		log.Fatal(err)
-	} else {
-		log.Printf("repos count: %#v", count)
-	}
-
-	// MostRecent(limit int) ([]Repo, error)
-	// MostStars(limit int) ([]Repo, error)
-	// Count() (int, error)
-	// MostPopularLanguage(limit int) ([]LanguageCount, error)
-	// RepoCountByUser(limit int) ([]UserCount, error)
-	// LanguageCountByUser(login string, limit int) ([]LanguageCount, error)
-	// MostRecentReposByLanguage(language string, limit int) ([]Repo, error)
-	// ReposByLanguage(language string, limit int) ([]Repo, error)
-
-	// if repos, err := rsvc.MostRecent(20); err != nil {
-	// 	log.Println("error", err)
-	// } else {
-	// 	log.Printf("repos: %#v", repos)
-	// }
-	// if count, err := usvc.MostRecent(20); err != nil {
-	// 	log.Println("error", err)
-	// } else {
-	// 	log.Printf("user count: %#v", count)
-	// }
-
 	// Setup workers
 	w := worker.New(gsvc, asvc, usvc, rsvc, logger)
+	_ = w
 	// c1 := w.NewFetchUsers("*/20 * * * * *")
 	// c1.Start()
 	// c2 := w.NewFetchRepos("*/20 * * * * *")
 	// c2.Start()
-	c3 := w.NewAnalyticBuilder("*/20 * * * * *")
-	c3.Start()
+	// c3 := w.NewAnalyticBuilder("*/20 * * * * *")
+	// c3.Start()
+
+	// Setup router
+	r := httprouter.New()
+
+	// Setup profiling
+	r.HandlerFunc(http.MethodGet, "/debug/pprof/", pprof.Index)
+	r.HandlerFunc(http.MethodGet, "/debug/pprof/cmdline", pprof.Cmdline)
+	r.HandlerFunc(http.MethodGet, "/debug/pprof/profile", pprof.Profile)
+	r.HandlerFunc(http.MethodGet, "/debug/pprof/symbol", pprof.Symbol)
+	r.HandlerFunc(http.MethodGet, "/debug/pprof/trace", pprof.Trace)
+	r.Handler(http.MethodGet, "/debug/pprof/goroutine", pprof.Handler("goroutine"))
+	r.Handler(http.MethodGet, "/debug/pprof/heap", pprof.Handler("heap"))
+	r.Handler(http.MethodGet, "/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
+	r.Handler(http.MethodGet, "/debug/pprof/block", pprof.Handler("block"))
 
 	// Setup endpoints
-	r := httprouter.New()
 	usersvc.MakeEndpoints(usvc, r)
 	analyticsvc.MakeEndpoints(asvc, r)
 	reposvc.MakeEndpoints(rsvc, r)
 
+	// Setup server
 	server := util.NewHTTPServer(*port, r)
 	log.Printf("listening to port *%s. press ctrl + c to cancel.\n", *port)
 	log.Fatal(server.ListenAndServe())
